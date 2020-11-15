9917b315d838c361eaff8de19c2611e519e6a003
Caoimhe Ní Chaoimh <caoimhe@oakreef.ie>
2020-11-15 14:25:14 +0000

    Cleanup/refactoring

--- snolf.lua
+++ snolf.lua
@@ -1,17 +1,21 @@
 freeslot("SPR_SFAH", "SPR_SFAV", "SPR_SFMR")
 
 hud.add(function(v, player, camera)
+	-- Don't do anything if we're not Snolf
 	if player.mo.skin == "snolf" then
 		v.drawString(16, 164, "SHOTS", V_YELLOWMAP)
 		v.drawString(64, 164, player.snolf.shots)
 
 		if player.snolf.state == 1 or player.snolf.state == 2 then
-			local meter = v.getSpritePatch(SPR_SFMR)
-			local harrow = v.getSpritePatch(SPR_SFAH)
-			local varrow = v.getSpritePatch(SPR_SFAV)
+			local meter = v.getSpritePatch(SPR_SFMR)  -- shot meter sprite
+			local harrow = v.getSpritePatch(SPR_SFAH) -- shot meter arrow sprite 1
+			local varrow = v.getSpritePatch(SPR_SFAV) -- shot meter arrow sprite 2
 
-			local hpos = player.snolf.convert_angle(player.snolf.hdrive, player.snolf.h_meter_length)
-			local vpos = player.snolf.convert_angle(player.snolf.vdrive, player.snolf.v_meter_length)
+			local h_meter_length = 50 -- how many pixels wide the charge meter range is
+			local v_meter_length = 50 -- how many pixels tall the charge meter range is
+
+			local hpos = player.snolf.convert_angle(player.snolf.hdrive, h_meter_length)
+			local vpos = player.snolf.convert_angle(player.snolf.vdrive, v_meter_length)
 
 			v.drawScaled(
 				FRACUNIT*158,
@@ -46,15 +50,13 @@ addHook("PreThinkFrame", function()
 		end
 
 		if player.snolf == nil then
-			player.snolf = { shots = 0 }
+			player.snolf = { shots = 0, state = 0 }
 			player.snolf.convert_angle = function (angle, max_val)
-				return sin(angle - ANGLE_90)
-					*max_val/FRACUNIT/2
-					+ max_val/2
+				return sin(angle - ANGLE_90)*max_val/FRACUNIT/2 + max_val/2
 			end
 		end
 
-		--check if the jump button was just tapped
+		-- check if the jump button was just tapped
 		if not (player.cmd.buttons & BT_JUMP) then
 			player.snolf.jumptapready = true
 			player.snolf.jumptapping = false
@@ -65,14 +67,14 @@ addHook("PreThinkFrame", function()
 			player.snolf.jumptapping = false
 		end
 
-		--check if the ability button is being held
-		if player.snolf.spinheld != nil and (player.cmd.buttons & BT_USE) then
+		-- check if the ability button is being held
+		if player.snolf.spinheld ~= nil and (player.cmd.buttons & BT_USE) then
 			player.snolf.spinheld = $1 + 1
 		else
 			player.snolf.spinheld = 0
 		end
 
-		--swallow player input
+		-- swallow player input
 		player.cmd.forwardmove = 0
 		player.cmd.sidemove = 0
 		player.cmd.buttons = $1 & !BT_JUMP
@@ -83,19 +85,29 @@ end)
 
 
 addHook("ThinkFrame", function()
+	-- Don't do anything if we're not Snolf
 	for player in players.iterate do
 		if player.mo.skin ~= "snolf" then
 			continue
 		end
 
-		player.snolf.max_hrz = 50 --max horizontal release speed
-		player.snolf.max_vrt = 50 --max vertical release speed
-		player.snolf.h_meter_length = 50 --how many pixels wide the charge meter range is
-		player.snolf.v_meter_length = 50 --how many pixels tall the charge meter range is
+		-- snolfstate
+		-- 0 ready to snolf
+		-- 1 snolfing horizontal
+		-- 2 snolfing vertical
+		-- 3 snolf'd
+
+		local max_hrz = 50 --max horizontal release speed
+		local max_vrt = 50 --max vertical release speed
+
+
+		-- I want the meter timing to be sinusoidal so we will be using trigonometry
+		local increment = ANG1 + ANG2
+		local max_charge = ANGLE_180
 
 		player.mo.state = S_PLAY_ROLL --force rolling animation
 
-		if player.snolf.spinheld > 60 and player.snolf.mull and player.speed != 0 then
+		if player.snolf.spinheld > 60 and player.snolf.mull and player.speed ~= 0 then
 			P_TeleportMove(player.mo,
 				player.snolf.mull.x,
 				player.snolf.mull.y,
@@ -105,7 +117,6 @@ addHook("ThinkFrame", function()
 			player.snolf.spinheld = 0
 			S_StartSound(player.mo, sfx_mixup)
 		end
-
 
 		if P_IsObjectOnGround(player.mo) then
 			player.pflags = $1 | PF_SPINNING --force spinning flag
@@ -119,18 +130,7 @@ addHook("ThinkFrame", function()
 			end
 		end
 
-		-- snolfstate
-		-- 0 ready to snolf
-		-- 1 snolfing horizontal
-		-- 2 snolfing vertical
-		-- 3 snolf'd
-
-
-		-- I want the meter timing to be sinusoidal so we will be using trigonometry
-		local increment = ANG1 + ANG2
-		local max_charge = ANGLE_180
-
-		if player.snolf.state == 0 then
+		if player.snolf.state == 0 then -- state 0: can start a shot
 			if player.snolf.jumptapping then
 				player.snolf.state = 1
 				player.snolf.hdrive = 0
@@ -138,7 +138,7 @@ addHook("ThinkFrame", function()
 				player.snolf.increment = increment
 				S_StartSoundAtVolume(player.mo, sfx_spndsh, 64)
 			end
-		elseif player.snolf.state == 1 then
+		elseif player.snolf.state == 1 then -- state 1: picking horizontal force
 			if player.snolf.jumptapping then
 				player.snolf.state = 2
 				player.snolf.increment = increment
@@ -153,13 +153,13 @@ addHook("ThinkFrame", function()
 
 				player.snolf.hdrive = $1 + player.snolf.increment
 			end
-		elseif player.snolf.state == 2 then
+		elseif player.snolf.state == 2 then -- state 2: picking vertical force
 			if player.snolf.jumptapping then
 				player.snolf.shots = $1 + 1
 				player.snolf.state = 3
 
-				local hspeed = player.snolf.convert_angle(player.snolf.hdrive, player.snolf.max_hrz)
-				local vspeed = player.snolf.convert_angle(player.snolf.vdrive, player.snolf.max_vrt)
+				local hspeed = player.snolf.convert_angle(player.snolf.hdrive, max_hrz)
+				local vspeed = player.snolf.convert_angle(player.snolf.vdrive, max_vrt)
 
 				P_InstaThrust(player.mo, player.mo.angle, hspeed*FRACUNIT)
 				P_SetObjectMomZ(player.mo, vspeed*FRACUNIT)
@@ -175,7 +175,7 @@ addHook("ThinkFrame", function()
 
 				player.snolf.vdrive = $1 + player.snolf.increment
 			end
-		elseif player.snolf.state == 3 then
+		elseif player.snolf.state == 3 then -- state 3: we have launched and can't do anything till we come to a stop
 			if P_IsObjectOnGround(player.mo) and player.speed == 0 then
 				player.snolf.state = 0
 			end
