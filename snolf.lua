c9b7ba5055033103d18297c4d70ec76130931959
Caoimhe Ní Chaoimh <caoimhe@oakreef.ie>
2020-12-05 20:22:50 +0000

    Drawing charge meter

--- snolf.lua
+++ snolf.lua
@@ -14,6 +14,9 @@ shot_ready = function(snolf_table)
 	repeat
 		coroutine.yield()
 	until (snlf.ctrl.jmp == 1)
+	snlf.charging = true
+	snlf.hdrive = -1
+	snlf.vdrive = -1
 	snlf.routine = coroutine.create(horizontal_charge, snlf)
 end
 
@@ -21,7 +24,6 @@ end
 horizontal_charge = function(snolf_table)
 	local snlf = snolf_table
 	local increment = 1
-	snlf.hdrive = -1
 	repeat
 		if snlf.hdrive >= h_meter_length then
 			increment = -1
@@ -39,7 +41,6 @@ end
 vertical_charge = function(snolf_table)
 	local snlf = snolf_table
 	local increment = 1
-	snlf.vdrive = -1
 	repeat
 		if snlf.vdrive >= v_meter_length then
 			increment = -1
@@ -52,8 +53,26 @@ vertical_charge = function(snolf_table)
 	until(snlf.ctrl.jmp == 1)
 	P_InstaThrust(snlf.mo, snlf.mo.angle, snlf.hdrive*FRACUNIT)
 	P_SetObjectMomZ(snlf.mo, snlf.vdrive*FRACUNIT)
+	snlf.charging = false
 	snlf.routine = coroutine.create(shot_ready, snlf)
 end
+
+
+-- draw the charge meter
+hud.add( function(v, player, camera)
+	if not player.mo or not player.snolf then return end
+	if not player.snolf.charging then return end
+
+	local meter = v.getSpritePatch(SPR_SFMR)  -- shot meter sprite
+	local harrow = v.getSpritePatch(SPR_SFAH, 0, 4) -- shot meter arrow sprite 1
+	local varrow = v.getSpritePatch(SPR_SFAV, 0, 5) -- shot meter arrow sprite 2
+
+	v.draw(158, 103, meter)
+	v.draw(160+player.snolf.hdrive, 151, harrow)
+	if player.snolf.vdrive ~= -1 then
+		v.draw(159, 150-player.snolf.vdrive, varrow)
+	end
+end, "game")
 
 
 -- store all snolf-relevant state info in player_t.snolf
@@ -61,7 +80,7 @@ local snolf_setup = function(player)
 	player.snolf = {
 		p = player,
 		mo = player.mo,
-		state = SNLF_RDY,
+		charging = false,
 		ctrl = { jmp = 0 },
 		handle_jump = handle_jump,
 		hdrive = 0,
