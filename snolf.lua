27c1d16f226be90ce9fd6bc96134ce1cb84e6b5a
Caoimhe Ní Chaoimh <caoimhe@oakreef.ie>
2021-04-13 19:25:54 +0100

    Fix the save state thing I just broke

--- snolf.lua
+++ snolf.lua
@@ -211,10 +211,10 @@ take_a_mulligan = function(snlf, pts, dont_play_sound)
 		if lm.hdrive ~= nil then snlf.hdrive = lm.hdrive end
 		if lm.vdrive ~= nil then snlf.vdrive = lm.vdrive end
 		if lm.chargegoingback ~= nil then snlf.chargegoingback = lm.chargegoingback end
-		if lm.flags ~= nil then mo.flags end
-		if lm.flags2 ~= nil then mo.flags2 end
-		if lm.eflags ~= nil then mo.eflags end
-		if lm.flags ~= nil then snlf.p.pflags end
+		if lm.flags ~= nil then mo.flags = lm.flags end
+		if lm.flags2 ~= nil then mo.flags2 = lm.flags2 end
+		if lm.eflags ~= nil then mo.eflags = lm.eflags end
+		if lm.flags ~= nil then snlf.p.pflags = lm.flags end
 
 		if snlf.p.pflags & PF_FINISHED == 0 then
 			snlf.mullcount = $1 + 1
