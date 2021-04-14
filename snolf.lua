5c71be91346450c46f8d7c67e3cf8e9477f05d34
Caoimhe Ní Chaoimh <caoimhe@oakreef.ie>
2021-04-14 14:08:36 +0100

    Add check for gemmasmash to bounce code

--- snolf.lua
+++ snolf.lua
@@ -765,7 +765,8 @@ addHook("PreThinkFrame", function()
 
 			-- if going fast enough when Snolf hits the ground, bounce
 			if abs(snlf.prev.momz) > BOUNCE_LIMIT and p.playerstate ~= PST_DEAD
-			and mo.state ~= S_PLAY_BOUNCE and mo.state ~= S_PLAY_BOUNCE_LANDING then
+			and mo.state ~= S_PLAY_BOUNCE and mo.state ~= S_PLAY_BOUNCE_LANDING
+			and not (p.gemmasmash and p.gemmasmash > 0) then
 				P_SetObjectMomZ(mo, FixedMul(snlf.prev.momz, BOUNCE_FACTOR) * (reversed_gravity(mo) and 1 or -1))
 				snlf.p.pflags = $1 | PF_JUMPED | PF_THOKKED | PF_SHIELDABILITY
 				-- move slightly off the ground immediately so snolf doesn't
